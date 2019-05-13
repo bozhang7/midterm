2dfb09ce9b23026296c86ebc15eb942b1c59a9fe
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-05-13 16:34:50 -0400

    Defined a default route that display a header and a link to a survey.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    // turns on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    // requires autoload file
+    require_once('vendor/autoload.php');
+
+    // instantiates f3
+    $f3 = Base::instance();
+
+    // turn on f3 error reporting
+    $f3->set('DEBUG', 3);
+
+    // defines a default route
+    $f3->route('GET /', function() {
+        echo '<h1>Midterm Survey</h1>';
+        echo '<a href="">Take my Midterm Survey</a>';
+    });
+
+    $f3->run();
